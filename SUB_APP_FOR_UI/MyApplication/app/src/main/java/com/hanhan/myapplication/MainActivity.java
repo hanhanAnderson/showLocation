c7204de08675f85ec734f8f69efe91d9b00852b9
HanhanAnderson <Hanhan@gwu.edu>
2019-04-18 19:56:53 -0400

    UI part

---
+++ SUB_APP_FOR_UI/MyApplication/app/src/main/java/com/hanhan/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.hanhan.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView mWebview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebview = (WebView) findViewById(R.id.webView);
+        mWebview.loadUrl("https://www.google.com/");
+
+        mWebview.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+
+    }
+}
